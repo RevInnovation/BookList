ef6453aab6712333f43d131bd8e6bf3041dc7c60
aldi <iskandar.aldi88@gmail.com>
2021-03-13 22:21:05 +0800

    adding applayer installer

---
+++ Boilerplate/ApplicationLayer/Books/BookService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boilerplate.ApplicationLayer.Books
+{
+    public class BookService : IBookService
+    {
+    }
+}
---
+++ BookList/AppStarts/Installers/ApplicationLayerInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Boilerplate.ApplicationLayer.Books;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetCore.AutoRegisterDi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Boilerplate.Webservice.AppStarts.Installers
+{
+    public class ApplicationLayerInstaller : IInstaller
+    {
+        public void InstallServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.RegisterAssemblyPublicNonGenericClasses(assemblies: Assembly.GetAssembly(typeof(IBookService)))
+                .Where(x => !x.IsInterface && !x.IsAbstract && x.Name.EndsWith("Service") && x.Namespace.Contains("ApplicationLayer"))
+                .AsPublicImplementedInterfaces(ServiceLifetime.Transient);
+        }
+    }
+}
