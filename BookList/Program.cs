6011fd6f13b191deba44e54f2d197aa718ef33f9
aldi <iskandar.aldi88@gmail.com>
2021-03-13 22:12:55 +0800

    initial setup

--- BookList/Program.cs
+++ BookList/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace BookList
+namespace Boilerplate.Webservice
 {
     public class Program
     {
